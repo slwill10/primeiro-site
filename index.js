a9d75e5bf4926b69992671f00647c7d5fe720e18
willian santos <wsantosdelima44@gmail.com>
2022-11-22 16:12:35 -0300

    arquivo

---
+++ index.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora > 0 && hora < 12) {
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#7c8b7f'
+    } else if (hora >= 12 && hora <= 18 ) {
+        img.src = 'fototarde.png'
+        document.body.style.background = '#a4acde'
+    } else {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#1d1f1e'
+    }
+
+    
+
+}
